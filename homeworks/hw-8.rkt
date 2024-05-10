d376b4e07d4c6358974a46cea2d39925ac1b3a92
DeveloperMindset123 <adas006@citymail.cuny.edu>
2024-05-09 22:45:41 -0400

    added content for hw8

---
+++ homeworks/hw-8.rkt
@@ -0,0 +1,31 @@
+; Priotorize the homework problems in the following order --> hw 8, hw 10, h2 9 and then hw 11, additionally, look into the correct implementation of cons, car, cdr 
+
+
+
+; Eighth Homework Set
+; CSc 335
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Homework Problems
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+
+; 1.  Prove the correctness of the value function presented in the class notes.  Your argument should be an
+; induction on (the complexity of) the argument.  State clearly what assumptions you make.
+
+
+; 2.  Make all changes needed to allow the a-iexp calculator to work with numbers given in base 1.
+; Explain carefully how you will represent such numbers; design plus, times, and exponent procedures
+; for them.  Prove correctness of your functions.
+
+
+; 3.  Exercises 2.2, 2.3 and 2.4 in Abelson and Sussman.  Proofs are not necessary.
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
