8d09d8243c6946b68437b91f795dffcdfb9e2f66
Ayan Das <adas006@citymail.cuny.edu>
2024-05-22 21:13:15 -0400

    feat:added assigned exam files
    These are the exam files we were assigned during the entire semester, also contains some additional review materials.

---
+++ FinalExam/Exam Related/Midterm/Fall 2023 Midterms/MidtermExamSectionM.scm
@@ -0,0 +1,140 @@
+; CSc 335 Section M
+; Fall 2023
+
+; October 26
+
+; Midterm Exam
+
+; Professor Troeger
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; TYPE YOUR NAME HERE:
+; TYPE YOUR NAME HERE!!!!!!!
+
+; TYPE YOUR FULL EMAIL ADDRESS HERE:
+; TYPE YOUR FULL EMAIL ADDRESS HERE!!!!!!!!!!
+
+
+; (I will email your graded paper to this address - DO NOT use a gmail address, as gmail has in the past
+; blocked name.scm and name.rkt files.  Please use your citymail address.)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; SAVE YOUR EXAM AS Lastname.Firstname.scm
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; INSTRUCTIONS 
+
+;; AFTER YOU DOWNLOAD THE EXAM
+
+; type your name and email address, above
+
+; save your file ON THE DESKTOP as lastname.firstname.scm
+
+; read and sign and date the honesty pledge, below (REQUIRED!)
+
+; start solving the exam problems as soon as you have signed and dated the honesty pledge
+
+; AT PRECISELY 12:20, stop writing and upload your exam
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Collaboration of any kind is not permitted: you are to work alone, with your ONLY reference
+; the R5RS language manual.  SEARCHING FOR ANSWERS ON THE WEB IS CHEATING.  CONTRACTING WITH CHEGGS (ETC)
+; TO SOLVE THE PROBLEM IS CHEATING.  WORKING WITH ANYONE ELSE IN PERSON OR VIA SOCIAL MEDIA IS CHEATING.
+; CONSULTING NOTES OR A BOOK IS CHEATING. DO NOT CHEAT!!!
+
+; Even the sight of a phone or a tablet will result in a grade of 0 on this exam: PUT YOUR DEVICES AWAY. 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; READ AND SIGN AND DATE:
+
+; READ: I pledge my honor that the work submitted here is entirely my own: I have not cheated on this exam.
+
+; SIGNATURE: 
+
+; DATE:
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;; Scoring Template - Do NOT Erase
+
+;;;; Explanation of Overall Approach and Functional Decomposition (5 points)
+
+;;;; Recursive Development and Procedure (10 points possible)
+
+;;;; Iterative Development and Procedure (10 points possible)
+
+;;;; Quality of Secondary Helper Functions (5 points possible)
+
+
+
+;;;; Total Score and Percentage (30 points possible, but graded on the basis of 25 points)
+
+;;;; Letter Grade
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; You have full access to drracket for this exam. All problems are to be solved using R5RS within drracket,
+; and using only language features discussed (non-casually) so far in the context of the quizzes or homework:
+
+; no vectors, no lists, no strings, no assignment, no loop, no letrec , no  ...
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; YOU SHOULD INSERT YOUR SOLUTION INTO THIS DOCUMENT, IMMEDIATELY FOLLOWING THE PROBLEM STATEMENT.
+
+; BE SURE TO SAVE YOUR WORK FREQUENTLY.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+; There is just one problem, with three parts, as indicated abovd. 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Specify, design, develop, code, prove correct and test an R5RS scheme procedure
+
+;     MERGESORT
+
+; which inputs a single non-negative integer n and which returns the integer m formed from n by
+; sorting its digits. 
+
+; Your algorithm should follow the standard definition of mergesort.  Your solution must include (at least) one
+; fully developed recursive procedure, and (at least) one fully developed iterative procedure.  If your main
+; procedure is recursive, then the primary helper function must be iterative; if your main procedure is iterative,
+; then the primary helper function must be recursive. You are likely to need more than one helper - you may choose
+; recursion or iteration for these as you see fit.
+
+; As for the quizzes, and as discussed in class, your grade will depend in largest part on your displayed
+; mastery of the program development techniques used so far in this course. 
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
